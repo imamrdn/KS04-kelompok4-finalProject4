4dfbbac20fbb923a79c9533e5d6b25e984dd3026
Imam Ramadhan <imamrmdn99@gmail.com>
2022-12-16 23:19:22 +0700

    modified : photo test

--- __tests__/photo.test.js
+++ __tests__/photo.test.js
@@ -45,9 +45,8 @@ describe("Test Photo Sukses", () => {
       });
     userId = jwt.decode(token);
     userId = userId.id;
-    console.log(token);
   });
-
+  //=======================================================================
   it("Create photo", async () => {
     const res = await request(app)
       .post("/photos/")
@@ -59,13 +58,21 @@ describe("Test Photo Sukses", () => {
         UserId: userId,
       });
     expect(res.statusCode).toEqual(201);
+    expect(res.body).not.toBeNull();
+    expect(res.body).toBeDefined();
+    expect(res.body).toBeTruthy();
+    expect(res.body).toEqual(expect.any(Object));
   });
-
+  //=======================================================================
   it("Read photo", async () => {
     const res = await request(app).get("/photos/").set("x-access-token", token);
     expect(res.statusCode).toEqual(200);
+    expect(res.body).not.toBeNull();
+    expect(res.body).toBeDefined();
+    expect(res.body).toBeTruthy();
+    expect(res.body).toEqual(expect.any(Object));
   });
-
+  //=======================================================================
   it("Update photo", async () => {
     const res = await request(app)
       .put("/photos/1")
@@ -76,27 +83,129 @@ describe("Test Photo Sukses", () => {
         poster_image_url: "http://iniurlbaru.com",
       });
     expect(res.statusCode).toEqual(201);
+    expect(res.body).not.toBeNull();
+    expect(res.body).toBeDefined();
+    expect(res.body).toBeTruthy();
+    expect(res.body).toEqual(expect.any(Object));
   });
-
+  //=========================================================================
   it("Delete photo", async () => {
     const res = await request(app)
       .delete("/photos/1")
       .set("x-access-token", token);
     expect(res.statusCode).toEqual(200);
+    expect(res.body).not.toBeNull();
+    expect(res.body).toBeDefined();
+    expect(res.body).toBeTruthy();
+    expect(res.body).toEqual(expect.any(Object));
   });
 });
+//============================================================================
 
+/* Creating a photo for the user. */
+request(app).post("/photos/").set("x-access-token", token).send({
+  title: "ini judul",
+  caption: "ini caption",
+  poster_image_url: "https://iniurl.com",
+  UserId: userId,
+});
+//============================================================================
 describe("Test Photo Error", () => {
-  it("Create photo Error not found", async () => {
-    const res = await request(app)
-      .post("/photo/")
-      .set("x-access-token", token)
+  // Create //======================================
+  test("Harusnya Error 500", async () => {
+    const {body} = await request(app)
+      .post("/photos/")
+      .set("x-access-token", `${token}`)
       .send({
         title: "ini judul",
         caption: "ini caption",
-        poster_image_url: "https://iniurl.com",
-        UserId: userId,
-      });
-    expect(res.statusCode).toEqual(404);
+        poster_image_url: "awokwokwokwokwok ",
+        UserId: "userId",
+      })
+      .expect(500);
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/Please try again/i);
+    expect(body).toMatch(
+      /Validation error: Wrong URL format. Please try again/i
+    );
+  });
+  test("Harusnya error 404", async () => {
+    const {body} = await request(app)
+      .post("/photo/")
+      .set("x-access-token", `${token}`)
+      .send({
+        title: "ini judul",
+        caption: "ini caption",
+        poster_image_url: "http://image.com/defaultphoto.png ",
+        UserId: "userId",
+      })
+      .expect(404);
+    expect(body).not.toBeNull();
+  });
+
+  // Read  //==========================================
+  test("Harusnya error 401", async () => {
+    const {body} = await request(app).get("/photos/").expect(401);
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/not authenticated/i);
+  });
+
+  // Update //========================================
+  test("Harusnya error 500", async () => {
+    const {body} = await request(app)
+      .delete("/photos/1")
+      .set("x-access-token", `bukanBoken`)
+      .expect(500);
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/jwt malformed. Please try again/i);
+  });
+
+  test("Harusnya error 401", async () => {
+    const {body} = await request(app).delete("/photos/").expect(401);
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/not authenticated/i);
+  });
+
+  test("Harusnya error 400", async () => {
+    const {body} = await request(app)
+      .delete(`/photos/${null}`)
+      .set("x-access-token", `${token}`)
+      .expect(400);
+
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/anda tidak memiliki akses/i);
+  });
+
+  // Delete //===========================================
+  test("Harusnya error 400", async () => {
+    const {body} = await request(app)
+      .delete("/photos/10")
+      .set("x-access-token", `${token}`)
+      .expect(400);
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/anda tidak memiliki akses/i);
+  });
+
+  test("Harusnya error 401", async () => {
+    const {body} = await request(app).delete("/photos/").expect(401);
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/not authenticated/i);
+  });
+
+  test("Harusnya error 500", async () => {
+    const {body} = await request(app)
+      .delete(`/photos/${null}`)
+      .set("x-access-token", `${token}`)
+      .expect(400);
+    expect(body).not.toBeNull();
+    expect(body).toEqual(expect.any(String));
+    expect(body).toMatch(/anda tidak memiliki akses/i);
   });
 });
